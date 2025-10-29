822d0f9d0b7aaf67d57847afef4c5953164db7eb
Nikita Budyuk <57536202+Budyuk99@users.noreply.github.com>
2025-10-29 16:57:29 +0300

    Added slider

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,40 @@
 // Находим кнопку и контейнер dropdown
-const langBtn = document.querySelector('.lang-dropdown');
-const langDropdown = document.querySelector('.lang-dropdown');
+const langBtn = document.querySelector(".lang-dropdown");
+const langDropdown = document.querySelector(".lang-dropdown");
 
 // Открытие/закрытие меню при клике на кнопку
-langBtn.addEventListener('click', (e) => {
+langBtn.addEventListener("click", (e) => {
     e.stopPropagation(); // чтобы клик не срабатывал на документе
-    langDropdown.classList.toggle('active');
+    langDropdown.classList.toggle("active");
 });
 
 // Закрытие меню при клике вне dropdown
-document.addEventListener('click', () => {
-    langDropdown.classList.remove('active');
+document.addEventListener("click", () => {
+    langDropdown.classList.remove("active");
+});
+
+$(".gallery-slider").owlCarousel({
+    loop: true,
+    margin: 30,
+    center: true,
+    nav: true,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1,
+            margin: 10,
+        },
+        768: {
+            items: 1.2, // немного выглядывает
+            margin: 20,
+        },
+        1200: {
+            items: 1.3, // ещё чуть больше выглядывают
+            margin: 40,
+        },
+        1600: {
+            items: 1.5, // при Full HD видно соседние
+            margin: 60,
+        },
+    },
 });
